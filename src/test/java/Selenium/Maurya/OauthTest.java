32de01b18fa3e6dbcfd11768261b1ed7f565b644
Santosh Maurya <mauryasantosh@outlook.in>
2021-05-04 15:23:12 +0530

    POJO

--- src/test/java/Selenium/Maurya/OauthTest.java
+++ src/test/java/Selenium/Maurya/OauthTest.java
@@ -1,6 +1,8 @@
 package Selenium.Maurya;
 import org.testng.annotations.Test;
 
+import Pojo.GetCourses;
+import io.restassured.parsing.Parser;
 import io.restassured.path.json.JsonPath;
 import static io.restassured.RestAssured.given;
 
@@ -10,7 +12,7 @@ public class OauthTest {
 	public void oauthTest()
 	{	//get code from this url-
 		// https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.googleapis.com/auth/userinfo.email&auth_url=https://accounts.google.com/o/oauth2/v2/auth&client_id=692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com&response_type=code&redirect_uri=https://rahulshettyacademy.com/getCourse.php
-		String url ="https://rahulshettyacademy.com/getCourse.php?code=4%2F0AY0e-g4qujUl-Kp7RPaxjh43m-VhVCZy9PAmMBi25P-zo3OoQk0YJJ68_e9prc6Yz9wh1A&scope=email+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email+openid&authuser=0&prompt=none#";
+		String url ="https://rahulshettyacademy.com/getCourse.php?code=4%2F0AY0e-g6UEhHBVvlmtXvEUSs2rMltz8Xs3lXLGwcTChaHdrQlaMAfVUDkFC62i_l0g0LVdg&scope=email+openid+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email&authuser=0&prompt=none#";
 
 		String partialcode=url.split("code=")[1];
 		String code=partialcode.split("&scope")[0];
@@ -30,11 +32,14 @@ public class OauthTest {
 	   		.post("https://www.googleapis.com/oauth2/v4/token")
 	   .then().log().all().extract().asString();
 		
-		JsonPath js =new JsonPath(res);
+		JsonPath js =new JsonPath(res);			
 		
-		given().contentType("application/json").queryParam("access_token",js.getString("access_token"))
-		.when().log().all().get("https://rahulshettyacademy.com/getCourse.php")
-		.then().log().all();
+		//Use of Pojo
+		GetCourses getCourses=
+				given().contentType("application/json").queryParams("access_token", js.getString("access_token")).expect().defaultParser(Parser.JSON)
+				.when().get("https://rahulshettyacademy.com/getCourse.php").as(GetCourses.class);
+		
+		System.out.println(getCourses.getInstructor());
 		
 			
 	}
